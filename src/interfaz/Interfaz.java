50ea12e6f66cce716a8c7517817b4e8cba6d0c17
Gastón <gaelceart@gmail.com>
2022-08-24 16:21:30 -0300

    interfaz v0.1

--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -15,7 +15,9 @@ public class Interfaz {
 
 	private JFrame frame;
 	private JLabel [][] tablero; 
-		int posLetra = 0;
+	private int posLetra = 0;
+	private int posFila = 0;
+	private LogicGame game;
 	/**
 	 * Launch the application.
 	 */
@@ -44,6 +46,7 @@ public class Interfaz {
 	 */
 	private void initialize() {
 		//Pantalla principal
+		game = new LogicGame(5);
 		frame = new JFrame();
 		frame.setBounds(100, 100, 524, 574);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -53,25 +56,87 @@ public class Interfaz {
 		crearTablero(5);
 		
 		//keyListener
-		int posIntento = 0;
-		
 		frame.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyTyped(KeyEvent e) {
-				if (!esTeclaValida(e))
+				System.out.println("Posicion letra: " + posLetra);
+				if (e.getKeyChar() == 8) { 
+					System.out.println("AAAAAAAAAAAAAAAAAaaa");
+					borrarLetra();
+					return;
+				}
+				
+				if (e.getKeyChar() == 10 && posLetra >= 4) {
+					enviarPalabra();
+					return;
+				}
+
+				if (!esTeclaValida(e) || posLetra > 4)
 					return;
 				
 				System.out.println(0 + e.getKeyChar());
-				tablero[0][posLetra++].setText("" + e.getKeyChar());
+				if (e.getKeyChar() != 8 && e.getKeyChar() != 10)
+					colocarLetra(e);
+				
+				
+			}
 
+			private void borrarLetra() {
+				System.out.println("ESTOY ACAAAA");
+				
+				if (posLetra > 5){
+					posLetra = 4;
+				}
+				
+				if (posLetra != 0 && tablero[posFila][posLetra].getText() == " " ) {
+					posLetra--;
+				}
+				tablero[posFila][posLetra].setText(" ");
+			}
+			
+			private void enviarPalabra() {
+				System.out.println("Su Uber flash está en camino!");
+				char[] palabraEnviada = new char[5];
+				for (int i = 0; i < tablero[posFila].length; i++) {
+					palabraEnviada[i] = tablero[posFila][i].getText().charAt(0);
+				}
+				if (game.terminarIntento(palabraEnviada)) {
+					//GANASTE!!!
+				}
+				if (posFila == 5) {
+					//perdiste
+					return;
+				}
+				posFila++;
+				posLetra = 0;
+				
 			}
 
 			private boolean esTeclaValida(KeyEvent e) {
 				//ascii 65 - 90 (209 = Ñ | 241 ñ) 97 - 122
-				return e.getKeyChar() == 209 || e.getKeyChar() == 241 ||
+				return e.getKeyChar() == 10 || e.getKeyChar() == 8 || e.getKeyChar() == 209 || e.getKeyChar() == 241 ||
 					  e.getKeyChar() >= 65 && e.getKeyChar() <= 90
 					  || e.getKeyChar() >= 97 && e.getKeyChar() <= 122;
 			}
+			
+			private void colocarLetra(KeyEvent e) {
+				
+				if (tablero[posFila][posLetra].getText() != " ")
+					return;
+				
+				char letra = mayus(e.getKeyChar());
+				tablero[posFila][posLetra].setText("" + letra);
+				posLetra += posLetra != tablero[0].length - 1 ? 1 : 0;
+				
+			}
+
+			private char mayus(char letra) {
+				if (letra >= 97 && letra <= 122 || letra == 241)
+					letra = (char) (letra - 32);
+				return letra;
+			}
+			
+			
 		});
 	
 		
@@ -86,7 +151,7 @@ public class Interfaz {
 		for (int f = 0; f < 6; f++) {
 			for (int c = 0; c <tamanoPalabra ; c++) {
 				tablero[f][c] = new JLabel();
-				tablero[f][c].setFont(new Font("Source Core Pro", Font.PLAIN, 64));
+				tablero[f][c].setFont(new Font("System", Font.PLAIN, 64));
 				tablero[f][c].setText(" ");
 				tablero[f][c].setBackground(Color.WHITE);
 				tablero[f][c].setOpaque(true);
