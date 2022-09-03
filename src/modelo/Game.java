834a2110407f2dd04f976b65edd643b6427219ea
Gastón <gaelceart@gmail.com>
2022-09-03 14:24:28 -0300

    format

--- src/modelo/Game.java
+++ src/modelo/Game.java
@@ -10,23 +10,30 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Game{
+public class Game {
 	private String palabraEnJuego = "";
 	private Map<Character, Integer> letraYcantidad;
 	private List<String> listadoDePalabras;
-	private EstadoCasillero [] resultadoLetras;
-	public enum EstadoCasillero{verde,amarillo,gris,vacio};
-	private enum Dificultad{facil,normal,dificil};
-	//modificar constructor para que tome la dificultad
+	private EstadoCasillero[] resultadoLetras;
+
+	public enum EstadoCasillero {
+		verde, amarillo, gris, vacio
+	};
+
+	private enum Dificultad {
+		facil, normal, dificil
+	};
+
+	// modificar constructor para que tome la dificultad
 	public Game(int tamanoPalabra) {
 		this.listadoDePalabras = new LinkedList<String>();
 		obtenerConjuntoDePalabras(Dificultad.normal, false);
 		seleccionarPalabra();
 		setearLetraYCantidad();
-		setearResultadosLetras();	
+		setearResultadosLetras();
 		System.out.println(palabraEnJuego);
 	}
-	
+
 	public boolean terminarIntento(char[] palabra) {
 		for (EstadoCasillero estLet : resultadoLetras) {
 			if (estLet != EstadoCasillero.verde) {
@@ -35,30 +42,32 @@ public class Game{
 				return false;
 			}
 		}
-		//acerto la palabra
+		// acerto la palabra
 		return true;
 	}
-	public EstadoCasillero[] aciertosJugador(char [] palabraIntento) {
-		//prioridad verde
+
+	public EstadoCasillero[] aciertosJugador(char[] palabraIntento) {
+		// prioridad verde
 		for (int i = 0; i < palabraEnJuego.length(); i++) {
 			if (palabraIntento[i] == palabraEnJuego.charAt(i)) {
 				resultadoLetras[i] = EstadoCasillero.verde;
-				letraYcantidad.put(palabraIntento[i], letraYcantidad.get(palabraIntento[i])-1);
+				letraYcantidad.put(palabraIntento[i], letraYcantidad.get(palabraIntento[i]) - 1);
 			}
 		}
-		//amarillo y gris
+		// amarillo y gris
 		for (int i = 0; i < palabraEnJuego.length(); i++) {
-			if (palabraIntento[i] != palabraEnJuego.charAt(i) && letraYcantidad.containsKey(palabraIntento[i]) && letraYcantidad.get(palabraIntento[i]) > 0 ) {
+			if (palabraIntento[i] != palabraEnJuego.charAt(i) && letraYcantidad.containsKey(palabraIntento[i])
+					&& letraYcantidad.get(palabraIntento[i]) > 0) {
 				resultadoLetras[i] = EstadoCasillero.amarillo;
-				letraYcantidad.put(palabraIntento[i], letraYcantidad.get(palabraIntento[i])-1);
-			}
-			else if(palabraIntento[i] != palabraEnJuego.charAt(i) && !letraYcantidad.containsKey(palabraIntento[i]) ||(palabraIntento[i] != palabraEnJuego.charAt(i) && letraYcantidad.get(palabraIntento[i]) == 0) ){
+				letraYcantidad.put(palabraIntento[i], letraYcantidad.get(palabraIntento[i]) - 1);
+			} else if (palabraIntento[i] != palabraEnJuego.charAt(i) && !letraYcantidad.containsKey(palabraIntento[i])
+					|| (palabraIntento[i] != palabraEnJuego.charAt(i) && letraYcantidad.get(palabraIntento[i]) == 0)) {
 				resultadoLetras[i] = EstadoCasillero.gris;
 			}
 		}
 		return this.resultadoLetras;
 	}
-	
+
 	public boolean perteneceAlListado(char[] palabra) {
 		StringBuilder palabraFormada = new StringBuilder();
 		for (int i = 0; i < palabra.length; i++) {
@@ -68,24 +77,28 @@ public class Game{
 	}
 
 	public boolean esTeclaValida(KeyEvent e) {
-		//ascii 65 - 90 (209 = Ñ | 241 ñ) 97 - 122
-		return e.getKeyChar() == 10 || e.getKeyChar() == 8 || e.getKeyChar() == 209 || e.getKeyChar() == 241 ||
-			  e.getKeyChar() >= 65 && e.getKeyChar() <= 90
-			  || e.getKeyChar() >= 97 && e.getKeyChar() <= 122;
+		// ascii 65 - 90 (209 = Ñ | 241 ñ) 97 - 122
+		return e.getKeyChar() == 10 || e.getKeyChar() == 8 || e.getKeyChar() == 209 || e.getKeyChar() == 241
+				|| e.getKeyChar() >= 65 && e.getKeyChar() <= 90 || e.getKeyChar() >= 97 && e.getKeyChar() <= 122;
 	}
+
 	public char mayus(char letra) {
 		if (letra >= 97 && letra <= 122 || letra == 241)
 			letra = (char) (letra - 32);
 		return letra;
 	}
-	
+
 	private void obtenerConjuntoDePalabras(Dificultad dificultad, boolean ingles) {
 		StringBuilder ruta = new StringBuilder(Game.class.getResource("").getPath());
-		ruta.append((ingles)?"ingles":"espanol");
-		if (dificultad == Dificultad.facil) {ruta.append("Facil.txt");}
-		else if(dificultad == Dificultad.normal) {ruta.append("Normal.txt");}
-		else {ruta.append("Dificil.txt");}
-		//leer contenido
+		ruta.append((ingles) ? "ingles" : "espanol");
+		if (dificultad == Dificultad.facil) {
+			ruta.append("Facil.txt");
+		} else if (dificultad == Dificultad.normal) {
+			ruta.append("Normal.txt");
+		} else {
+			ruta.append("Dificil.txt");
+		}
+		// leer contenido
 		File archivoPalabras = new File(ruta.toString());
 		try {
 			Scanner palabras = new Scanner(archivoPalabras);
@@ -93,14 +106,16 @@ public class Game{
 				listadoDePalabras.add(palabras.next().toString().toLowerCase());
 			}
 			palabras.close();
-		} catch (FileNotFoundException e) {	
-				e.printStackTrace();
-			}	
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
+
 	private void seleccionarPalabra() {
 		Random random = new Random();
-		this.palabraEnJuego = listadoDePalabras.get(random.nextInt(listadoDePalabras.size()-1));
+		this.palabraEnJuego = listadoDePalabras.get(random.nextInt(listadoDePalabras.size() - 1));
 	}
+
 	private void setearLetraYCantidad() {
 		letraYcantidad = new HashMap<Character, Integer>();
 		palabraEnJuego = palabraEnJuego.toUpperCase();
@@ -109,6 +124,7 @@ public class Game{
 			letraYcantidad.put(c, cantExistente + 1);
 		}
 	}
+
 	private void setearResultadosLetras() {
 		this.resultadoLetras = new EstadoCasillero[palabraEnJuego.length()];
 		for (int i = 0; i < resultadoLetras.length; i++) {
