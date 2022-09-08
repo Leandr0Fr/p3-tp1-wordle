1d7678a6b398f4f422914d579b1cbe7e4999cf59
Leandr0Fr <leandro00fr@gmail.com>
2022-09-08 14:59:25 -0300

    actualizacion interfaz

--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -1,31 +1,12 @@
 package interfaz;
 
-<<<<<<< HEAD
-import modelo.LogicGame;
-=======
 import modelo.Game;
 import modelo.Game.EstadoCasillero;
 import ranking.Ranking;
->>>>>>> pre-main
 
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
-<<<<<<< HEAD
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-
-public class Interfaz {
-
-	private JFrame frame;
-	private JLabel [][] tablero; 
-	private int posLetra = 0;
-	private int posFila = 0;
-	private LogicGame game;
-=======
 import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.JButton;
@@ -64,7 +45,6 @@ public class Interfaz {
 	private Ranking rkNormal;
 	private Ranking rkDificil;
 
->>>>>>> pre-main
 	/**
 	 * Launch the application.
 	 */
@@ -92,30 +72,6 @@ public class Interfaz {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-<<<<<<< HEAD
-		//Pantalla principal
-		game = new LogicGame(5,"Menem");
-		frame = new JFrame();
-		frame.setBounds(100, 100, 524, 574);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		//tablero	
-		crearTablero(5);
-		
-		//keyListener
-		frame.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-				System.out.println("Posicion letra: " + posLetra);
-				if (e.getKeyChar() == 8) { 
-					System.out.println("AAAAAAAAAAAAAAAAAaaa");
-					borrarLetra();
-					return;
-				}
-				
-				if (e.getKeyChar() == 10 && posLetra >= 4) {
-=======
 		// Pantalla principal
 		LEN_PALABRA = 5;
 		recurso = new Recurso();
@@ -167,109 +123,10 @@ public class Interfaz {
 
 				if (e.getKeyChar() == KeyEvent.VK_ENTER && posLetra >= LEN_PALABRA - 1
 						&& tablero[posFila][LEN_PALABRA - 1].getText() != " ") {
->>>>>>> pre-main
 					enviarPalabra();
 					return;
 				}
 
-<<<<<<< HEAD
-				if (!esTeclaValida(e) || posLetra > 4)
-					return;
-				
-				System.out.println(0 + e.getKeyChar());
-				if (e.getKeyChar() != 8 && e.getKeyChar() != 10)
-					colocarLetra(e);
-				
-				
-			}
-
-			private void borrarLetra() {
-				System.out.println("ESTOY ACAAAA");
-				
-				if (posLetra > 5){
-					posLetra = 4;
-				}
-				
-				if (posLetra != 0 && tablero[posFila][posLetra].getText() == " " ) {
-					posLetra--;
-				}
-				tablero[posFila][posLetra].setText(" ");
-			}
-			
-			private void enviarPalabra() {
-				System.out.println("Su Uber flash está en camino!");
-				char[] palabraEnviada = new char[5];
-				for (int i = 0; i < tablero[posFila].length; i++) {
-					palabraEnviada[i] = tablero[posFila][i].getText().charAt(0);
-				}
-				if (game.terminarIntento(palabraEnviada)) {
-					//GANASTE!!!
-				}
-				if (posFila == 5) {
-					//perdiste
-					return;
-				}
-				posFila++;
-				posLetra = 0;
-				
-			}
-
-			private boolean esTeclaValida(KeyEvent e) {
-				//ascii 65 - 90 (209 = Ñ | 241 ñ) 97 - 122
-				return e.getKeyChar() == 10 || e.getKeyChar() == 8 || e.getKeyChar() == 209 || e.getKeyChar() == 241 ||
-					  e.getKeyChar() >= 65 && e.getKeyChar() <= 90
-					  || e.getKeyChar() >= 97 && e.getKeyChar() <= 122;
-			}
-			
-			private void colocarLetra(KeyEvent e) {
-				
-				if (tablero[posFila][posLetra].getText() != " ")
-					return;
-				
-				char letra = mayus(e.getKeyChar());
-				tablero[posFila][posLetra].setText("" + letra);
-				posLetra += posLetra != tablero[0].length - 1 ? 1 : 0;
-				
-			}
-
-			private char mayus(char letra) {
-				if (letra >= 97 && letra <= 122 || letra == 241)
-					letra = (char) (letra - 32);
-				return letra;
-			}
-			
-			
-		});
-	
-		
-		
-	}
-
-	private void crearTablero(int tamanoPalabra){
-		int x = 60;
-		int y = 30;
-		int CENTER = 0;
-		tablero = new JLabel[6][tamanoPalabra];
-		for (int f = 0; f < 6; f++) {
-			for (int c = 0; c <tamanoPalabra ; c++) {
-				tablero[f][c] = new JLabel();
-				tablero[f][c].setFont(new Font("System", Font.PLAIN, 64));
-				tablero[f][c].setText(" ");
-				tablero[f][c].setBackground(Color.WHITE);
-				tablero[f][c].setOpaque(true);
-				tablero[f][c].setHorizontalAlignment(CENTER);
-				tablero[f][c].setBounds(x,y,64,64);
-				frame.getContentPane().add(tablero[f][c]);
-				x+= 72;
-			}
-			y+= 72;
-			x = 60;			
-		}
-	}
-}
-
-	
-=======
 				if (!game.esTeclaValida(e) || posLetra > LEN_PALABRA - 1)
 					return;
 
@@ -418,4 +275,3 @@ public class Interfaz {
 		frame.requestFocus();
 	}
 }
->>>>>>> pre-main
